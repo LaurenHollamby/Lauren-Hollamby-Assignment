943edb5528704107b671fa9a6f0dae6609628907
LaurenHollamby <lauren.hollamby@edu.sait.ca>
2024-11-18 11:57:42 -0700

    dynamic year

---
+++ about.js
@@ -0,0 +1,10 @@
+
+
+// dynamic year
+
+const fYear = document.getElementById("footerYear");
+const d = new Date ();
+fYear.innerText = d.getFullYear();
+['']
+//fYear.textContext = d
+console.log(d);
